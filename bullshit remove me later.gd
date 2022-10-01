7fba005d0f4f8cd3b7805e3b45db87aa2af2ba29
PandaUnwise <benjamin.aarli@hotmail.com>
2022-10-01 04:22:48 +0200

    added more bullshit

--- bullshit remove me later.gd
+++ bullshit remove me later.gd
@@ -9,6 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("I am the bullshit that will be removed, hopefully.")
+	print("dupe: I am the bullshit that will be removed, hopefully.")
 	pass # Replace with function body.
 
 
